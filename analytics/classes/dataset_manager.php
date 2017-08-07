192e8ad933d11127ad34a7444ddf7abd32f5d82e
David Monllao <davidm@moodle.com>
2017-08-07 12:15:19 +0200

    MDL-59067 analytics: Store indicator calculations
    This was supposed to be split into multiple commits to make it easier to understand
    but I failed to do it properly. So this is the list of changes:
    
    - New analytics_indicator_calc db table to store indicators calculations
    - Reuse previous calculations during prediction/training; other models
      previous calculations should also be reused as long as they belong to
      the same sample (sampleid depends on sampleorigin), time range and indicator
    - Allow bulk inserting of these calculations as this can hurt database performance
    - Block the same analysable to be analysed for training and for prediction
    - Use a new instance of the target and use it for is_valid_* functions
      as using ::is_valid_sample can lead to problems if people
      uses it to cache stuff

--- analytics/classes/dataset_manager.php
+++ analytics/classes/dataset_manager.php
@@ -107,9 +107,11 @@ class dataset_manager {
      * @return bool Could we get the lock or not.
      */
     public function init_process() {
+
+        // Do not include $this->includetarget as we don't want the same analysable to be analysed for training
+        // and prediction at the same time.
         $lockkey = 'modelid:' . $this->modelid . '-analysableid:' . $this->analysableid .
-            '-timesplitting:' . self::clean_time_splitting_id($this->timesplittingid) .
-            '-includetarget:' . (int)$this->includetarget;
+            '-timesplitting:' . self::clean_time_splitting_id($this->timesplittingid);
 
         // Large timeout as processes may be quite long.
         $lockfactory = \core\lock\lock_config::get_lock_factory('core_analytics');
