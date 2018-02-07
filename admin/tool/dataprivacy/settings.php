0e1b7d8add77fb863c97d544634abdb9c43cc206
David Monllao <davidm@moodle.com>
2018-02-07 13:40:50 +0100

    MDL-61362 tool_dataprivacy: Base to access editing features

--- admin/tool/dataprivacy/settings.php
+++ admin/tool/dataprivacy/settings.php
@@ -58,3 +58,7 @@ if (!empty($roles)) {
 $ADMIN->add('dataprivacysettings', new admin_externalpage('datarequests', get_string('datarequests', 'tool_dataprivacy'),
     new moodle_url('/admin/tool/dataprivacy/datarequests.php'), 'tool/dataprivacy:managedatarequests')
 );
+
+$ADMIN->add('dataprivacysettings', new admin_externalpage('dataregistry', get_string('dataregistry', 'tool_dataprivacy'),
+    new moodle_url('/admin/tool/dataprivacy/dataregistry.php'), 'tool/dataprivacy:managedatarequests')
+);
